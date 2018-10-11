c2afea271cf72cafd306a3cadb2ad0909dcb40a7
Volodymyr Khreptak <34722004+villaclara@users.noreply.github.com>
2018-10-11 12:42:07 +0300

    Add files via upload

---
+++ MainTestTask.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="MainTestTask.aspx.cs" Inherits="MainTestTask" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        	Choose text:<br />
+            <asp:FileUpload ID="FileUploadControl" runat="server" />
+            <asp:Button runat="server" id="ButtonBrowse" text="Browse" onclick="ButtonBrowse_Click" /> 
+			<br />
+            <asp:Label ID="Label3" runat="server" Text="Label"></asp:Label>
+            <br />
+			<br />
+			Enter word:<br />
+			<asp:TextBox ID="TextBoxWord" runat="server"></asp:TextBox>
+			<asp:Button ID="ButtonSearchWord" runat="server" Text="Search" OnClick="ButtonSearchWord_Click" />
+			<br />
+			<asp:Label ID="Label4" runat="server" Text="Label"></asp:Label>
+			<br />
+			<br />
+            <br />
+			<asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" DataSourceID="SqlDataSource1">
+                <Columns>
+                    <asp:BoundField DataField="label" HeaderText="label" SortExpression="label" />
+                    <asp:BoundField DataField="sentence" HeaderText="sentence" SortExpression="sentence" />
+                    <asp:BoundField DataField="count" HeaderText="count" SortExpression="count" />
+                </Columns>
+			</asp:GridView>
+        </div>
+        <p>
+            <asp:Label ID="Label2" runat="server" Text="Label"></asp:Label>
+            <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+        </p>
+        <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:TestTaskConnectionString %>" SelectCommand="SELECT * FROM [TestTaskTable]"></asp:SqlDataSource>
+    </form>
+</body>
+</html>
---
+++ MainTestTask.aspx.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.IO;
+using System.Text.RegularExpressions;
+
+
+public partial class MainTestTask : System.Web.UI.Page
+{
+    // imya faily
+    private string filename = null;
+    string filename2 = null;
+    StreamReader s;
+
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Label1.Visible = false;
+        Label2.Visible = false;
+        Label3.Text = "";
+        Label4.Text = "";
+    }
+
+    // get file and save
+    protected void ButtonBrowse_Click(object sender, EventArgs e)
+    {
+        if (FileUploadControl.HasFile)
+        {
+            try
+            {
+                filename = Path.GetFileName(FileUploadControl.FileName);
+                FileUploadControl.SaveAs(Server.MapPath("~/") + filename);
+                string path = Server.MapPath("~/" + filename);
+                Label1.Text = filename;
+                Label2.Text = path;
+                filename2 = filename;
+                Label3.Text = "OK <br> Filename - " + Label1.Text ;
+
+            }
+            catch (Exception)
+            {
+                Response.Write("Error.Cannot get file");
+            }
+        }
+    }
+
+
+    // searches words
+    protected void ButtonSearchWord_Click(object sender, EventArgs e)
+    {
+
+        // SQLCONNECTION
+        // SQL OPEN
+        // SQL QUERY
+        // SQL COMMAND
+        SqlConnection sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["TestTaskConnectionString"].ConnectionString);
+        sqlConnection.Open();
+        string query = "insert into TestTaskTable (label, sentence, count) values (@label, @sentence, @count)";
+        SqlCommand command = new SqlCommand(query, sqlConnection);
+
+
+        // WORD - slovo yake treba shykatu
+        string word = TextBoxWord.Text;
+        
+ 
+        
+            //string path = Server.MapPath("~/" + filename);
+            StreamReader s = new StreamReader(Label2.Text);
+         
+           
+            string text = s.ReadToEnd();
+
+
+            // split text + search if contain word
+            string[] sentences = text.Split('.');
+            string reversed;
+            int count = 0;
+            foreach (var sentence in sentences)
+            {
+                count = 0;
+                if (sentence.Contains(word))
+                {
+                    // kilkist' vxodzhen' slova v 1 rechennya
+                    foreach (Match match in Regex.Matches(sentence, word))
+                    {
+                        count++;
+                    }
+
+
+                    // zvorotni bykvu
+                    reversed = ReverseLetter(sentence);
+
+
+                    // zapus v db
+                    command.Parameters.AddWithValue("@label", Label1.Text);
+                    command.Parameters.AddWithValue("@sentence", reversed);
+                    command.Parameters.AddWithValue("@count", count);
+                    command.ExecuteNonQuery();
+                    command.Parameters.Clear();
+                }
+            }
+            string query1 = "SELECT * FROM TestTaskTable";
+            SqlCommand command1 = new SqlCommand(query1, sqlConnection);
+            SqlDataReader reader;
+            reader = command1.ExecuteReader();
+
+            s.Close();
+        Label4.Text = "Reboot program to see changes.";
+            sqlConnection.Close();
+
+        
+
+    }
+    // zvorotnij napryamok bykv
+    string ReverseLetter(string text)
+    {
+        // dlya ostann'ogo slova
+        text += " ";
+
+
+        string reversed = string.Empty;
+        // POS - current posucia kyrsora v rechenni
+        int pos = 0;
+        for (int i = 0; i < text.Length; i++)
+        {
+            if (text[i] == ' ')
+            {
+                string tmp = text.Substring(pos, i - pos);
+                reversed += new string(tmp.Reverse().ToArray()) + " ";
+                pos = i + 1;
+            }
+        }
+
+        
+
+        return reversed;
+    }
+}
